12be07b59f00e9a49a528e08546a9f58deabb681
Sag67 <sagarsada29@gmail.com>
2025-09-28 08:45:52 +0530

    Clean repo: keep only medication reminder app files

--- 2nd sem/python 2month/userinput.py
+++
@@ -1,6 +0,0 @@
-# name=input(" wht is your name")
-# color=input(' which color do u like')
-# print(name+"likes this color "+color)
-weight=input(" enter your weight")
-kg=int(weight)*1000
-print(kg)
